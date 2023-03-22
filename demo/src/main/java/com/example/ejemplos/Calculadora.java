7e0dc97b2055d3963ff10ca013d4ff7a9ee4f016
iudoming <iudoming@host.docker.internal>
2023-03-22 14:53:24 +0100

    Test Calculadora +

--- demo/src/main/java/com/example/ejemplos/Calculadora.java
+++ demo/src/main/java/com/example/ejemplos/Calculadora.java
@@ -1,8 +1,15 @@
 package com.example.ejemplos;
 
 public class Calculadora {
-	public double suma(double a, double b)
-	{
-		return a+b;
+	public double suma(double a, double b) {
+		return a + b;
+	}
+
+	public double divide(double a, double b) {
+		return a / b;
+	}
+
+	public double divide(int a, int b) {
+		return a / b;
 	}
 }
--- demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
+++ demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
@@ -12,48 +12,57 @@ class CalculadoraTest {
 	}
 
 	@Test
-	void testSuma() {
-		
+	void testSumaPositivoPositivo() {
 		var calc = new Calculadora();
 		
 		var rslt = calc.suma(2, 2);
 		
 		assertEquals(4, rslt);
-		
 	}
-	
+
 	@Test
 	void testSumaPositivoNegativo() {
-		
 		var calc = new Calculadora();
 		
-		var rslt = calc.suma(3, -1);
+		var rslt = calc.suma(1, -0.9);
 		
-		assertEquals(2, rslt);
-		
+		assertEquals(0.1, rslt);
 	}
-	
+
 	@Test
 	void testSumaNegativoPositivo() {
-		
 		var calc = new Calculadora();
 		
 		var rslt = calc.suma(-1, 5);
 		
 		assertEquals(4, rslt);
-		
-	}
-	
-	@Test
-	void testSumaDecimales() {
-		
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(0.2, 0.5);
-		
-		assertEquals(0.7, rslt);
-		
 	}
 
+	@Test
+	void testSumaNegativoNegativo() {
+		var calc = new Calculadora();
+		
+		var rslt = calc.suma(-1, -5);
+		
+		assertEquals(-6, rslt);
+	}
+
+	@Test
+	void testSumaDecimale() {
+		var calc = new Calculadora();
+		
+		var rslt = calc.suma(0.1, 0.2);
+		
+		assertEquals(0.3, rslt);
+	}
+
+	@Test
+	void testDividir() {
+		var calc = new Calculadora();
+		
+		var rslt = calc.divide(1, 0.0);
+		
+		assertEquals(0.5, rslt);
+	}
 
 }
