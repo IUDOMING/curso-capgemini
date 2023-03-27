a7a2b9d61aebe166ae99b7cc5d9ff8f528c48025
IUDOMING <IUDOMING@host.docker.internal>
2023-03-27 11:15:47 +0200

    Nif Validation + Nif Test Validation

--- demo/src/main/java/com/example/demo/ioc/Validation.java
+++ demo/src/main/java/com/example/demo/ioc/Validation.java
@@ -5,23 +5,23 @@ import java.util.regex.Pattern;
 
 public class Validation {
 
-	private static final Pattern REGEXP = Pattern.compile("[0-9]{8}[a-zA-Z]");
+	private static final Pattern REGEXP = Pattern.compile("[0-9]{8}[A-Z]");
 	private static final String DIGITO_CONTROL = "TRWAGMYFPDXBNJZSQVHLCKE";
-	private static final String DIGITO_CONTROL2 = "trwagmyfpdxbnjzsqvhlcke";
 	private static final String[] INVALIDOS = new String[] { "00000000T", "00000001R", "99999999R" };
-	
-	
-	
+
 	public Validation() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static boolean validarDNI(String dni) {
+
+		if (dni == null)
+			return true;
+		dni = dni.toUpperCase();
 		return Arrays.binarySearch(INVALIDOS, dni) < 0 // (1)
 				&& REGEXP.matcher(dni).matches() // (2)
-				&& (dni.charAt(8) == DIGITO_CONTROL.charAt(Integer.parseInt(dni.substring(0, 8)) % 23) // (3)
-				||	dni.charAt(8) == DIGITO_CONTROL2.charAt(Integer.parseInt(dni.substring(0, 8)) % 23));
-	
+				&& (dni.charAt(8) == DIGITO_CONTROL.charAt(Integer.parseInt(dni.substring(0, 8)) % 23)); // (3)
+
 	}
 
 }
--- demo/src/test/java/com/example/demo/ioc/ValidationTest.java
+++ demo/src/test/java/com/example/demo/ioc/ValidationTest.java
@@ -1,14 +1,16 @@
 package com.example.demo.ioc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import com.example.core.test.SpaceCamelCase;
 
@@ -27,15 +29,11 @@ class ValidationTest {
 		@Nested
 		class OK {
 
-			@Test
-			void comprobaciónIndividual() {
-				assertTrue(Validation.validarDNI("47927767E"));
-			}
-
-			@ParameterizedTest(name = "{0}")
+			@ParameterizedTest(name = "Caso: {0}")
 			@CsvSource(value = { "12345678Z", "45673254S", "47927767E", 
 								 "77619560B", "77619559X", "34570706G",
 								 "93259475h", "28859421x", "93237260k"})
+			@NullSource
 			void comprobaciónEnTanda(String nif) {
 				assertTrue(Validation.validarDNI(nif));
 			}
@@ -45,18 +43,13 @@ class ValidationTest {
 		@Nested
 		class KO {
 
-			@Test
-			void comprobacionCadenaVacia() {
-				assertFalse(Validation.validarDNI(""));
+
 			}
-			@Test
-			void comprobacionEspacio() {
-				assertFalse(Validation.validarDNI(" "));
-			}
-			@ParameterizedTest(name = "{0}")
+			@ParameterizedTest(name = "Caso: {0}")
 			@CsvSource(value = { "00000000T", "00000001R", "99999999R", "0123", 
 								 "01234a67Z", "012345678-", "0123456789",
 								 "B77619560", "ABCDEFGHY","ABCDE FGH9"})
+			@EmptySource
 			void comprobaciónInvalidosEnTanda(String nif) {
 				assertFalse(Validation.validarDNI(nif));
 			}
@@ -64,4 +57,4 @@ class ValidationTest {
 		}
 	}
 
-}
+
