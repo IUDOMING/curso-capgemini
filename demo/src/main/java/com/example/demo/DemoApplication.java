a8c29e3227784d8fe13cfc5f0231208ac4a0ad16
IUDOMING <IUDOMING@host.docker.internal>
2023-03-28 13:19:53 +0200

    Entities and BBDD practice

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,45 @@
 package com.example.demo;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
+import com.example.demo.domains.contracts.repositories.ActorRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
 	}
+
+	@Autowired
+	ActorRepository dao;
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 		System.out.println("Aplicación arrancada");
+//		(new EjemplosIoC()).run();
+//		var actor = new Actor(0, "Pepito", "grillo");
+//		dao.save(actor);
+//		dao.deleteById(215);
+//		var item = dao.findById(215);
+//		if(item.isPresent()) {
+//			var actor = item.get();
+//			actor.setLastName(actor.getLastName().toUpperCase());
+//			dao.save(actor);
+//			dao.findAll().forEach(System.out::println);
+//		} else {
+//			System.out.println("Actor no encontrado");
+//		}
+//		dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("P")
+//			.forEach(System.out::println);
+		dao.findTop5ByFirstNameStartingWith("P", Sort.by("LastName").descending())
+			.forEach(System.out::println);
+		dao.findTop5ByFirstNameStartingWith("P", Sort.by("FirstName"))
+		.forEach(System.out::println);
 	}
-
-
-
-	
 
 }
