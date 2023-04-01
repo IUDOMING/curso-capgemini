5acfb7fae64c003a4624a8876148d61adee43e42
iudoming <iudoming@host.docker.internal>
2023-04-01 13:26:35 +0200

    Cleaning main App

--- Catalogo/src/main/java/com/example/CatalogoApplication.java
+++ Catalogo/src/main/java/com/example/CatalogoApplication.java
@@ -1,14 +1,27 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.application.services.CatalogoService;
+import com.example.domains.contracts.services.LanguageService;
+import com.example.domains.entities.Language;
+
 @SpringBootApplication
-public class CatalogoApplication {
+public class CatalogoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
 		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+	
 	}
 
 }
