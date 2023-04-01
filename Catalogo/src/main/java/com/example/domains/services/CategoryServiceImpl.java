6823d2cccbc66e9471c2ff6c755ce946ffdecc98
iudoming <iudoming@host.docker.internal>
2023-04-01 12:26:59 +0200

    Services Implementation Creation

---
+++ Catalogo/src/main/java/com/example/domains/services/CategoryServiceImpl.java
@@ -0,0 +1,76 @@
+package com.example.domains.services;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.example.domains.contracts.repository.CategoryRepository;
+import com.example.domains.contracts.services.CategoryService;
+import com.example.domains.entities.Category;
+import com.example.exceptions.InvalidDataException;
+import com.example.exceptions.NotFoundException;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+	@Autowired
+	private CategoryRepository dao;
+
+	@Override
+	public List<Category> getAll() {
+		return dao.findAll(Sort.by("name"));
+	}
+
+	@Override
+	public Optional<Category> getOne(Integer id) {
+		return dao.findById(id);
+	}
+
+	@Override
+	public Category add(Category item) throws DuplicateKeyException, InvalidDataException {
+		if (item == null)
+			throw new InvalidDataException("Can't be null");
+		if (item.isInvalid())
+			throw new InvalidDataException(item.getErrorsMessage());
+		if (dao.existsById(item.getCategoryId()))
+			throw new DuplicateKeyException(item.getErrorsMessage());
+		return dao.save(item);
+	}
+
+	@Override
+	public Category modify(Category item) throws NotFoundException, InvalidDataException {
+		if (item == null)
+			throw new InvalidDataException("Can't be null");
+		if (item.isInvalid())
+			throw new InvalidDataException(item.getErrorsMessage());
+		if (getOne(item.getCategoryId()).isEmpty())
+			throw new NotFoundException();
+		return dao.save(item);
+
+	}
+
+	@Override
+	public void delete(Category item) throws InvalidDataException {
+		if (item == null)
+			throw new InvalidDataException("Can't be null");
+		deleteById(item.getCategoryId());
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		dao.deleteById(id);
+
+	}
+
+	@Override
+	public List<Category> novedades(Timestamp date) {
+		return dao.findByLastUpdateGreaterThanEqualOrderByLastUpdate(date);
+	}
+
+}
--- Catalogo/src/main/java/com/example/domains/services/FilmServiceImpl.java
+++ Catalogo/src/main/java/com/example/domains/services/FilmServiceImpl.java
@@ -11,19 +11,20 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import com.example.domains.contracts.repository.ActorRepository;
-import com.example.domains.contracts.services.ActorService;
-import com.example.domains.entities.Actor;
+import com.example.domains.contracts.repository.FilmRepository;
+import com.example.domains.contracts.services.FilmServices;
+import com.example.domains.entities.Film;
 import com.example.exceptions.InvalidDataException;
 import com.example.exceptions.NotFoundException;
 
+import jakarta.transaction.Transactional;
 import lombok.NonNull;
 
 @Service
-public class ActorServiceImpl implements ActorService {
+public class FilmServiceImpl implements FilmServices {
 
 	@Autowired
-	private ActorRepository dao;
+	private FilmRepository dao;
 
 	@Override
 	public <T> List<T> getByProjection(Class<T> type) {
@@ -41,53 +42,61 @@ public class ActorServiceImpl implements ActorService {
 	}
 
 	@Override
-	public Iterable<Actor> getAll(Sort sort) {
+	public Iterable<Film> getAll(Sort sort) {
 		return dao.findAll(sort);
 	}
 
 	@Override
-	public Page<Actor> getAll(Pageable pageable) {
+	public Page<Film> getAll(Pageable pageable) {
 		return dao.findAll(pageable);
 	}
 
 	@Override
-	public List<Actor> getAll() {
+	public List<Film> getAll() {
 		return dao.findAll();
 	}
 
 	@Override
-	public Optional<Actor> getOne(Integer id) {
+	public Optional<Film> getOne(Integer id) {
 		return dao.findById(id);
 	}
 
 	@Override
-	public Actor add(Actor item) throws DuplicateKeyException, InvalidDataException {
+	public Film add(Film item) throws DuplicateKeyException, InvalidDataException {
 		if (item == null)
 			throw new InvalidDataException("Can't be null");
 		if (item.isInvalid())
 			throw new InvalidDataException(item.getErrorsMessage());
-		if (dao.existsById(item.getActorId()))
+		if (dao.existsById(item.getFilmId()))
 			throw new DuplicateKeyException(item.getErrorsMessage());
-		return dao.save(item);
+		var actores = item.getActors();
+		var categorias = item.getCategories();
+		item.clearActors();
+		item.clearCategories();
+		var newItem = dao.save(item);
+		newItem.setActors(actores);
+		newItem.setCategories(categorias);
+		return dao.save(newItem);
 	}
 
 	@Override
-	public Actor modify(Actor item) throws NotFoundException, InvalidDataException {
-		if (item == null)
-			throw new InvalidDataException("No puede ser nulo");
-		if (item.isInvalid())
+	@Transactional
+	public Film modify(Film item) throws NotFoundException, InvalidDataException {
+		if(item == null)
+			throw new InvalidDataException("Can't be null");
+		if(item.isInvalid())
 			throw new InvalidDataException(item.getErrorsMessage());
-		if (dao.existsById(item.getActorId()))
+		var read = dao.findById(item.getFilmId());
+		if(read.isEmpty())
 			throw new NotFoundException();
-		return dao.save(item);
-
+		return dao.save(item.merge(read.get()));
 	}
 
 	@Override
-	public void delete(Actor item) throws InvalidDataException {
+	public void delete(Film item) throws InvalidDataException {
 		if (item == null)
-			throw new InvalidDataException("No puede ser nulo");
-		deleteById(item.getActorId());
+			throw new InvalidDataException("Can't be null");
+		deleteById(item.getFilmId());
 
 	}
 
@@ -98,7 +107,7 @@ public class ActorServiceImpl implements ActorService {
 	}
 
 	@Override
-	public List<Actor> novedades(@NonNull Timestamp date) {
+	public List<Film> novedades(@NonNull Timestamp date) {
 		return dao.findByLastUpdateGreaterThanEqualOrderByLastUpdate(date);
 	}
 
