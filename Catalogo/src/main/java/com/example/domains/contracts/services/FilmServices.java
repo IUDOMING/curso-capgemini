169ff51d7bd771bbebd7df5877bfa521adfaab50
iudoming <iudoming@host.docker.internal>
2023-04-01 12:26:33 +0200

    Wrong Object Usage Correction

--- Catalogo/src/main/java/com/example/domains/contracts/services/FilmServices.java
+++ Catalogo/src/main/java/com/example/domains/contracts/services/FilmServices.java
@@ -4,10 +4,9 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import com.example.domains.core.contracts.services.ProjectionDomainService;
-import com.example.domains.entities.Actor;
 import com.example.domains.entities.Film;
 
-public interface FilmServices extends ProjectionDomainService<Actor, Integer> {
+public interface FilmServices extends ProjectionDomainService<Film, Integer> {
 
 	List<Film> novedades(Timestamp fecha);
 
