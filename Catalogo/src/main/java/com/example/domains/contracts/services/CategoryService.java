bec055c13501524d78526f2c664e5d63876984e4
iudoming <iudoming@host.docker.internal>
2023-04-01 11:36:58 +0200

    Update contract.services

--- Catalogo/src/main/java/com/example/domains/contracts/services/CategoryService.java
+++ Catalogo/src/main/java/com/example/domains/contracts/services/CategoryService.java
@@ -3,10 +3,10 @@ package com.example.domains.contracts.services;
 import java.sql.Timestamp;
 import java.util.List;
 
-import com.example.domains.core.contracts.services.ProjectionDomainService;
+import com.example.domains.core.contracts.services.DomainService;
 import com.example.domains.entities.Category;
 
-public interface CategoryService extends ProjectionDomainService<Category, Integer> {
+public interface CategoryService extends DomainService<Category, Integer> {
 
 	List<Category> novedades(Timestamp fecha);
 
--- Catalogo/src/main/java/com/example/domains/contracts/services/LanguageRepository.java
+++ Catalogo/src/main/java/com/example/domains/contracts/services/LanguageRepository.java
@@ -3,10 +3,10 @@ package com.example.domains.contracts.services;
 import java.sql.Timestamp;
 import java.util.List;
 
-import com.example.domains.core.contracts.services.ProjectionDomainService;
+import com.example.domains.core.contracts.services.DomainService;
 import com.example.domains.entities.Language;
 
-public interface LanguageRepository extends ProjectionDomainService<Language, Integer> {
+public interface LanguageRepository extends DomainService<Language, Integer> {
 
 	List<Language> novedades(Timestamp fecha);
 
