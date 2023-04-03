15ec7fb6edb16821ba2f5bf18bf8eef204f923a1
IUDOMING <IUDOMING@host.docker.internal>
2023-04-03 10:10:03 +0200

    Language Test

--- Catalogo/src/test/java/com/example/domains/entities/LanguageTest.java
+++ Catalogo/src/test/java/com/example/domains/entities/LanguageTest.java
@@ -8,23 +8,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class LanguageTest {
-
 	@Test
 	void testIsValid() {
-		var fixture = new Language(0, "Mandarin");
+		var fixture = new Language(0, "Madarin");
 		assertTrue(fixture.isValid());
 	}
 
-	@DisplayName("Name must be between 2 and 45. Can't be blank.")
-	@ParameterizedTest(name = "fist name: -{0}- -> {1}")
-	@CsvSource(value = {
-			"'','ERRORES: firstName: size must be between 2 and 45, must not be blank.'", 
-			"' ','ERRORES: firstName: size must be between 2 and 45, must not be blank.'", 
-			"'   ','ERRORES: firstName: must not be blank.'", 
-			"A,'ERRORES: firstName: size must be between 2 and 45.'",
-			"12345678901234567890123456789012345678901234567890,'ERRORES: firstName: size must be between 2 and 45.'"})
-	
-	void testNombreIsInvalid(String value, String error) {
+	@DisplayName("Language must be between 2 and 20. Can't be blank.")
+	@ParameterizedTest(name = "language: -{0}- -> {1}")
+	@CsvSource(value = { "'','ERRORES: name: must not be blank.'", "' ','ERRORES: name: must not be blank.'",
+			"'   ','ERRORES: name: must not be blank.'",
+			"12345678901234567890123456789012345678901234567890,'ERRORES: name: size must be between 0 and 20.'" })
+	void testNameIsInvalid(String value, String error) {
 		var fixture = new Language(0, value);
 		assertTrue(fixture.isInvalid());
 		assertEquals(error, fixture.getErrorsMessage());
