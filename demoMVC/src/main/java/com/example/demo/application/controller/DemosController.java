0ff2d1ac6471a8d192089e230eb357bb0ccd0a40
iudoming <iudoming@host.docker.internal>
2023-04-04 10:23:54 +0200

    Update

--- demoMVC/src/main/java/com/example/demo/application/controller/DemosController.java
+++ demoMVC/src/main/java/com/example/demo/application/controller/DemosController.java
@@ -9,26 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// Anotación que indica controlador
 @Controller
-// Anotación para las rutas
 //@RequestMapping(path = "/")
 public class DemosController {
-	//Cuanto en la ruta venga un parametro id, se insertarán los valores en cada apartado
 	@GetMapping("/params/{id}")
 	@ResponseBody
 	public String cotilla(
-			@PathVariable String id, 
-			@RequestParam String nom,
-			@RequestHeader("Accept-Language") String language,
-			//Si es la primera vez que se ejecuta, JSESSIONID no vendrá
-			@CookieValue(required = false, name = "JSESSIONID") String cookie) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("id: " + id + "\n");
-		sb.append("nom: " + nom + "\n");
-		sb.append("language: " + language + "\n");
-		sb.append("cookie: " + cookie + "\n");
-		return sb.toString();
+	        @PathVariable int id,
+	        @RequestParam String nom,
+	        @RequestHeader("Accept-Language") String language, 
+	        @CookieValue(required=false, name = "JSESSIONID") String cookie) { 
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("id: " + id + "\n");
+	    sb.append("nom: " + nom + "\n");
+	    sb.append("language: " + language + "\n");
+	    sb.append("cookie: " + cookie + "\n");
+	    return sb.toString();
 	}
 
 }
