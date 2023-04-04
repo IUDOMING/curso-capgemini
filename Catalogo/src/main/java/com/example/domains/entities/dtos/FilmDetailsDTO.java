4fa7ced4efa6951211bd5848491956ec2b4b3f49
iudoming <iudoming@LES-5CG0517K3S.corp.capgemini.com>
2023-04-04 18:30:32 +0200

    Updated Constructors

--- Catalogo/src/main/java/com/example/domains/entities/dtos/FilmDetailsDTO.java
+++ Catalogo/src/main/java/com/example/domains/entities/dtos/FilmDetailsDTO.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import com.example.domains.entities.Film;
+import com.example.domains.entities.Film.Rating;
+import com.example.domains.entities.Language;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Value;
@@ -11,35 +13,47 @@ import lombok.Value;
 @Value
 public class FilmDetailsDTO {
 	private int filmId;
+	private String title;
 	private String description;
-	private int length;
-	private String rating;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy")
 	private Short releaseYear;
+	private Language language;
+	private Language languageVO;
 	private byte rentalDuration;
 	private BigDecimal rentalRate;
 	private BigDecimal replacementCost;
-	private String title;
-	private String language;
-	private String languageVO;
+	private int length;
+	private Rating rating;
 	private List<String> actors;
 	private List<String> categories;
 
 	public static FilmDetailsDTO from(Film source) {
 		return new FilmDetailsDTO(
 				source.getFilmId(),
-				source.getDescription(),
-				source.getLength(),
-				source.getRating().getValue(),
-				source.getReleaseYear(),
-				source.getRentalDuration(),
-				source.getRentalRate(),
-				source.getReplacementCost(),
 				source.getTitle(),
-				source.getLanguage() == null ? null : source.getLanguage().getName(),
-				source.getLanguageVO() == null ? null : source.getLanguageVO().getName(),
-				source.getActors().stream()
-				.map(item -> item.getFirstName() + " " + item.getLastName()).sorted().toList(),
-				source.getCategories().stream().map(item -> item.getName()).sorted().toList());
+				source.getDescription(),
+				source.getReleaseYear(),
+				source.getLanguage() == null ? null : source.getLanguage(),
+				source.getLanguageVO() == null ? null : source.getLanguageVO(),
+				source.getRentalDuration(), source.getRentalRate(),
+				source.getReplacementCost(),
+				source.getLength(),
+				source.getRating(),
+				source.getActors().stream().map(item-> item.getFirstName() + " " + item.getLastName()).sorted().toList(),
+				source.getCategories().stream().map(item->item.getName()).sorted().toList()
+				);
+	}
+	public static Film from(FilmDetailsDTO source) {
+		return new Film(
+				source.getFilmId(),
+				source.getTitle(),
+				source.getDescription(),
+				source.getReleaseYear(),
+				source.getLanguage() == null ? null : source.getLanguage(),
+				source.getLanguageVO() == null ? null : source.getLanguageVO(),
+				source.getRentalDuration(), source.getRentalRate(),
+				source.getLength(),
+				source.getReplacementCost(),
+				source.getRating());
 	}
 }
