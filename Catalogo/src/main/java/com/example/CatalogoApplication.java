bd280a60138b46a34f2c1624183819e8e50efcda
iudoming <iudoming@host.docker.internal>
2023-04-05 08:34:23 +0200

    Clean Main App

--- Catalogo/src/main/java/com/example/CatalogoApplication.java
+++ Catalogo/src/main/java/com/example/CatalogoApplication.java
@@ -1,13 +1,8 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.domains.contracts.repository.CategoryRepository;
-import com.example.domains.entities.Category;
-import com.example.domains.services.CategoryServiceImpl;
 
 @SpringBootApplication
 public class CatalogoApplication implements CommandLineRunner {
@@ -17,18 +12,7 @@ public class CatalogoApplication implements CommandLineRunner {
 
 	}
 
-	@Autowired
-	CategoryRepository dao;
-
-	@Autowired
-	CategoryServiceImpl srv;
-
 	public void run(String... args) throws Exception {
-
-		var c1 = new Category(0, "Adios");
-		srv.add(c1);
-		c1.setName("Buenas");
-		srv.modify(c1);
 
 	}
 }
