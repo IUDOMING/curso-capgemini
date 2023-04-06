52826939d65a8989b0328d56d5c342f31b0b5ce4
IUDOMING <IUDOMING@host.docker.internal>
2023-04-06 12:53:04 +0200

    ElementoDTO added

--- demoMVC/src/main/java/com/example/demo/domains/entities/dtos/ElementoDTO.java
+++ demoMVC/src/main/java/com/example/demo/domains/entities/dtos/ElementoDTO.java
@@ -1,13 +1,9 @@
 package com.example.demo.domains.entities.dtos;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Value;
 
 @Value
 public class ElementoDTO<K, V> {
-	@JsonProperty("id")
 	private K keyK;
-	@JsonProperty("titulo")
 	private V value;
 }
