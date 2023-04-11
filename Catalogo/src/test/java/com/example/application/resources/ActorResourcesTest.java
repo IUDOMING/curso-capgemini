2d081c2191dca6dce09cd08b90807353169f01ba
iudoming <iudoming@LES-5CG0517K3S.corp.capgemini.com>
2023-04-11 18:29:41 +0200

    ActorResourcesTest finished

--- Catalogo/src/test/java/com/example/application/resources/ActorResourcesTest.java
+++ Catalogo/src/test/java/com/example/application/resources/ActorResourcesTest.java
@@ -1,18 +1,26 @@
 package com.example.application.resources;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -24,6 +32,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.domains.contracts.services.ActorService;
 import com.example.domains.entities.Actor;
+import com.example.domains.entities.Film;
+import com.example.domains.entities.Film.Rating;
+import com.example.domains.entities.FilmActor;
+import com.example.domains.entities.Language;
 import com.example.domains.entities.dtos.ActorDTO;
 import com.example.domains.entities.dtos.ActorShort;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -42,16 +54,6 @@ class ActorResourcesTest {
 	@Autowired
 	ObjectMapper objectMapper;
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@Test
-	@Disabled
-	void test() {
-		fail("Not yet implemented");
-	}
-
 	@Value
 	static class ActorShortMock implements ActorShort {
 		int actorId;
@@ -59,6 +61,7 @@ class ActorResourcesTest {
 	}
 
 	@Test
+	@DisplayName("Get all actors")
 	void testGetAllString() throws Exception {
 		List<ActorShort> lista = new ArrayList<>(Arrays.asList(
 				new ActorShortMock(1, "Samuel JACSKON"),
@@ -73,6 +76,7 @@ class ActorResourcesTest {
 	}
 
 	@Test
+	@DisplayName("Get all actors with pages")
 	void testGetAllPageable() throws Exception {
 		List<ActorShort> lista = new ArrayList<>(Arrays.asList(
 				new ActorShortMock(1, "Samuel JACSKON"),
@@ -91,6 +95,7 @@ class ActorResourcesTest {
 
 	
 	@Test
+	@DisplayName("Get one actor")
 	void testGetOne() throws Exception {
 		int id = 1;
 		var ele = new Actor(id, "Samuel", "JACKSON");
@@ -104,16 +109,45 @@ class ActorResourcesTest {
 	}
 	
 	@Test
-	void testGetOne404() throws Exception {
-		int id = 1;
-		var ele = new Actor(id, "Samuel", "JACSKON");
-		when(srv.getOne(id)).thenReturn(Optional.empty());
-		mockMvc.perform(get("/api/actores/v1/{id}", id))
-				.andExpect(status().isNotFound())
-				.andExpect(jsonPath("$.title").value("Not Found")).andDo(print());
+	@DisplayName("Get all actor's films")
+	void testGetActorFilms() throws Exception{
+		var id = 1; 
+		List<Film> filmList = new ArrayList<>(
+		        Arrays.asList(new Film(1, "Titulo", "Description of the film we don't know about",
+		        		new Short("1990"), new Language(3),
+						new Language(5), (byte) 10, new BigDecimal(20),
+						230, new BigDecimal(20), Rating.GENERAL_AUDIENCES),
+		        		new Film(2, "Random Title", "Description of the random film",
+		        				new Short("1993"), new Language(3),
+						new Language(5), (byte) 10, new BigDecimal(20),
+						230, new BigDecimal(20), Rating.GENERAL_AUDIENCES)));
+		        		
+		var actor = new Actor(id, "Samuel","JACKSON");
+		
+		List<FilmActor> filmActors = new ArrayList<>(
+				Arrays.asList(
+						new FilmActor(filmList.get(0), actor),
+						new FilmActor(filmList.get(1), actor)
+						));
+				
+		
+		actor.setFilmActors(filmActors);
+		
+		when(srv.getOne(id)).thenReturn(Optional.of(actor));
+		
+		mockMvc.perform(get("/api/actores/v1/{id}/pelis", id))
+			.andExpectAll(
+					status().isOk(),
+					jsonPath("$[0].id").value(1),
+					jsonPath("$[0].title").value("Titulo"),
+					jsonPath("$[1].id").value(2),
+					jsonPath("$[1].title").value("Random Title")
+			).andDo(print());
+			
 	}
 
 	@Test
+	@DisplayName("Create Actor")
 	void testCreate() throws Exception {
 		int id = 1;
 		var ele = new Actor(id, "Samuel", "JACSKON");
@@ -126,23 +160,31 @@ class ActorResourcesTest {
 	}
 
 	@Test
-	void testUpdate()throws Exception {
+	@DisplayName("Update Actor")
+	void testUpdate() throws Exception {
 		int id = 1;
-		var ele = new Actor(id, "Samuel", "JACKSON");
-		when(srv.add(ele)).thenReturn(ele);
-		when(srv.getOne(id)).thenReturn(Optional.of(ele));
-		ele.setLastName("JASON");
-		mockMvc.perform(get("/api/actores/v1/{id}", id))
-			.andExpect(status().isOk())
-	        .andExpect(jsonPath("$.id").value(id))
-	        .andExpect(jsonPath("$.name").value(ele.getFirstName()))
-	        .andExpect(jsonPath("$.surname").value(ele.getLastName()))
+		var ele = new Actor(id,"Samuel", "JACKSON");
+		
+		when(srv.modify(ele)).thenReturn(ele);
+		
+		mockMvc.perform(put("/api/actores/v1/{id}", id)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(ActorDTO.from(ele))))
+			.andExpect(status().isNoContent())
 	        .andDo(print());
-}
-
-	@Test
-	void testDelete() {
-		fail("Not yet implemented");
 	}
 
+	@Test
+	@DisplayName("Delete actor")
+	void testDelete() throws Exception {
+		var id = 1;
+		
+		doNothing().when(srv).deleteById(id);
+		
+		mockMvc.perform(delete("/api/actores/v1/{id}", id))
+			.andExpect(status().isNoContent())
+	        .andDo(print());
+		
+		verify(srv,times(1)).deleteById(id);
+	}
 }
